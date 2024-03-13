da89d2ee464aef358082b1aea9761a56f77141d7
muskaancontlo <157454647+muskaancontlo@users.noreply.github.com>
2024-03-13 18:33:37 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,6 +9,9 @@ apt update
 apt install unzip 
 unzip cogagent-vqa.zip?download=true
 cd ..
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 mkdir data 
 cd data
 mkdir images
